3e8c3e569c67477db89406342a87167647681597
giraldi07 <giraldipramay@gmail.com>
2025-01-04 07:01:41 +0700

    build2x

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // Check if sw.js exists in the public directory
-    navigator.serviceWorker
-      .register('/sw.js')  // Ensure the sw.js file is in the root of public directory
-      .then(() => {
-        console.log('Service Worker registration successful');
-      })
-      .catch((err) => {
-        console.error('Service Worker registration failed: ', err);
-      });
-  });
-}
-
 // Ensure the element with id 'root' exists
 const rootElement = document.getElementById('root');
 if (rootElement) {
