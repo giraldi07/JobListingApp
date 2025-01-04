d0ce388e2cba8b443c9dddf1fa688dc3200c5f87
giraldi07 <giraldipramay@gmail.com>
2025-01-04 08:18:11 +0700

    sitemap integrate

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import  sitemap  from 'vite-plugin-sitemap'; // Pastikan ini adalah plugin yang benar
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://job-listing-app-puce.vercel.app/', // Tambahkan hostname
+      exclude: ['/some-page'], // Jika ada halaman yang ingin dikecualikan
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Alias untuk folder src
@@ -17,7 +24,7 @@ export default defineConfig({
     outDir: 'dist', // Output folder
     assetsDir: 'assets', // Folder untuk static assets dalam dist
   },
-  base: './', // Tambahkan base untuk memastikan path relatif pada build
+  base: './', // Base path relatif saat build
   server: {
     open: true, // Membuka browser otomatis saat menjalankan dev server
     port: 3000, // Port server
